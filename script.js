af13a2207de326d52da8aab4fdb488a601d3d868
Porter Smith <portersmith34@gmail.com>
2025-06-17 22:26:41 -0600

    fixed save bug

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addTodo() {
 listContainer.addEventListener('click', function (e) {
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle('complete');
-        saveData
+        saveData();
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
         saveData();
